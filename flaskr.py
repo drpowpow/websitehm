41a6392cd5ca09e2681cc726fb51eb107b1fe0e9
drpowpow <apowers@Alicias-MacBook-Pro.local>
2016-02-13 19:37:05 -0500

    fix file

--- flaskr.py
+++ flaskr.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 import mysql.connector
 from flask import Flask, request, session, g, redirect, url_for, \
      abort, render_template, flash
@@ -19,7 +20,7 @@ def connect_db():
 
 @app.route('/')
 def show_entries():
-    return render_template('about.html')
+    return render_template('templates/about.html')
 
 if __name__ == '__main__':
     app.run()
